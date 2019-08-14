9b9ebeb2280a61ebf31b02459fb0fb20e422bb21
cultureweb <cedric.milius@yahoo.fr>
2019-08-15 00:04:24 +0200

    error on schema

--- db/migrations/20190814225600_create_users_and_todos_tables.js
+++ db/migrations/20190814225600_create_users_and_todos_tables.js
@@ -1,6 +1,6 @@
-
+ 
 exports.up = function(knex, Promise) {
-  return knex.createTable('users', function(table) {
+  return knex.schema.createTable('users', function(table) {
       table.increments();
       table.string('name').notNullable();
       table.string('email').notNullable();
