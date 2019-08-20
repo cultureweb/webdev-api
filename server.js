641894fda751b81f28a7b3d0bacd966d3928221d
cultureweb <cedric.milius@yahoo.fr>
2019-08-20 22:08:18 +0200

    server ok

--- server.js
+++ server.js
@@ -5,8 +5,6 @@
 const app = express(); // Include body-parser
 const bodyParser = require("body-parser");
 
-/* const cors = require('cors') */
-
 const routes = require('./routes/todos')
 
 const PORT = process.env.PORT || 8000;
@@ -14,34 +12,14 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 const knex = require('./db/knex');
 
-// Configure headers
-/* app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
- */
 // Configure body-parser setting
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/* app.use(cors()) */
 
 // Routes
 app.use('/', routes)
 
 const logger = console
-/* 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(PORT, HOST, () => {
-  logger.info(`Running on http://${HOST}:${PORT}`);
-}); */
 
 const server = app
 .listen(parseInt(PORT, 10), HOST, () => {
