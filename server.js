6c8402914e243bba88727f2f814b9bdf0ecb5a74
cultureweb <cedric.milius@yahoo.fr>
2019-08-20 20:50:25 +0200

    Configure headers

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 const knex = require('./db/knex');
 
+// Configure headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Configure body-parser setting
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
