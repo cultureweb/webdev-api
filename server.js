f79c8804ac0f20526f88b2bc39a1c42cc9248c5d
cultureweb <35487627+cultureweb@users.noreply.github.com>
2019-08-21 14:23:53 +0200

    add headers en test

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const knex = require('./db/knex');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Configure Headers Tests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Routes
 app.use('/', routes)
 
